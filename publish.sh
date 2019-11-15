896f424b0eaa4dfe7435024534ee50e99bbceb94
pjt3591oo <pjt3591oo@gmail.com>
2019-11-15 18:45:25 +0900

    workflows/deploy.yml 수정

---
+++ publish.sh
@@ -0,0 +1,9 @@
+# build
+echo '======= build ======='
+set -x
+python3 setup.py bdist_wheel
+
+# upload
+echo '======= upload ======='
+set -x
+twine upload dist/Mung-1.0-py3-none-any.whl
---
+++ test.py
@@ -0,0 +1,8 @@
+from Mung import frog
+from Mung.House import computer
+
+n = frog.get_name()
+print(n)
+
+c = computer.show_computer()
+print(c)
